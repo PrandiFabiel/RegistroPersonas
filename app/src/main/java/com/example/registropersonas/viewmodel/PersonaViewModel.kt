f6e9c96ed07da20e395a419f9b4e0758f25e57ac
PrandiFabiel <fabiel098@gmail.com>
2022-05-07 11:37:00 -0400

    Creando personaViewModel.

---
+++ app/src/main/java/com/example/registropersonas/viewmodel/PersonaViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.registropersonas.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.registropersonas.data.PersonaDao
+import com.example.registropersonas.model.Persona
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PersonaViewModel @Inject constructor(
+    val  personaDao: PersonaDao
+) : ViewModel(){
+    private val _guardado = MutableLiveData(false)
+    val guardado: LiveData<Boolean> get() = _guardado
+
+    fun guardar(persona: Persona){
+        viewModelScope.launch {
+            personaDao.insertar(persona)
+            _guardado.value=true
+        }
+    }
+}
