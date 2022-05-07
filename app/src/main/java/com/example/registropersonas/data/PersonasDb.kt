f7345d80f9bee062ce4a4919fe0c32ab9fb2163c
PrandiFabiel <fabiel098@gmail.com>
2022-05-07 11:04:55 -0400

    Creando la base de datos(PersonaDb).

---
+++ app/src/main/java/com/example/registropersonas/data/PersonasDb.kt
@@ -0,0 +1,13 @@
+package com.example.registropersonas.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.registropersonas.model.Persona
+
+@Database(
+    entities = [Persona::class],
+    version = 1
+)
+abstract class PersonasDb: RoomDatabase() {
+    abstract val personaDao: PersonaDao
+}
