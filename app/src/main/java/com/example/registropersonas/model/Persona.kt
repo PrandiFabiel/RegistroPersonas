a0eb4facff50128e609ca3b0eb721af5b7707347
PrandiFabiel <fabiel098@gmail.com>
2022-05-17 14:41:53 -0400

    Agregando parcelize al modelo persona.

--- app/src/main/java/com/example/registropersonas/model/Persona.kt
+++ app/src/main/java/com/example/registropersonas/model/Persona.kt
@@ -1,12 +1,15 @@
 package com.example.registropersonas.model
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = "Personas")
+@Parcelize
 data class Persona(
     @PrimaryKey(autoGenerate = true)
     val personaId: Int,
     val nombres: String,
     val balance: Float
-)
+): Parcelable
